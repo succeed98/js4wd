0e01a3eaeaabb6966198baf7f7fa7abd7cefd777
Fourteen98 <emuhiydeen6@gmail.com>
2023-05-07 01:01:29 +0000

    complete HandsOnProject1-2

---
+++ Chapter01/HandsOnProject1-2/index.js
@@ -0,0 +1,19 @@
+/**
+  JavaScript 6th Edition
+      Chapter 1
+Hands - on Project 1 - 2
+
+Author:
+Date:
+
+Filename: index.js
+*/
+ console.log("working")
+let service1Name = "Basic";
+let service2Name = "Express";
+let service3Name = "Extreme";
+let service4Name = "Ultimate";
+let service1Speed = "0 Mbps";
+let service2Speed = "100 Mbps";
+let service3Speed = "500 Mbps";
+let service4Speed = "1 Gig";
